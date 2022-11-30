772baa3c5a76c4b272a75ebb061c46a9b57d04e0
Miku21 <rafaelfarizi1@gmail.com>
2022-11-30 07:17:39 +0000

    Update Auth.php

--- app/middleware/Auth.php
+++ app/middleware/Auth.php
@@ -3,13 +3,13 @@
     // session_start();
 
     class Auth {
-        // public function __invoke($req,$res,$next){
-        //     if(!isset($_SESSION['user'])){
-        //         $_SESSION['notLogin'] = true;
-        //         return $res->withRedirect('\login');
+        public function __invoke($req,$res,$next){
+            // if(!isset($_SESSION['user'])){
+            //     $_SESSION['notLogin'] = true;
+            //     return $res->withRedirect('\login');
 
-        //     }
-        //     return $next($req, $res);
-        // }
+            // }
+            return $next($req, $res);
+        }
     }
 ?>
