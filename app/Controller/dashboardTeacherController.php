97c5ae178aa9ab8767d829b1802abffd40951e6d
user <user@gitlocal>
2022-11-30 17:15:03 +0700

    translate sebagian

---
+++ app/Controller/dashboardTeacherController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+class dsahboardTeacherController
+{
+
+    public static function addNewTeacher($app, $request, $response, $args)
+    {
+        return 0;
+    }
+
+}
