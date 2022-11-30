0be1db463a111e458a7bcfad7cc1d8e40aaf6e2e
Miku21 <rafaelfarizi1@gmail.com>
2022-11-30 18:19:28 +0700

    update DashboarParent tagihan by catur

--- app/Controller/DashbordParentConroller.php
+++ app/Controller/DashbordParentConroller.php
@@ -11,19 +11,23 @@ class DashbordParentConroller
         $type = $args['type'];
         $id_parent = $args['id_parent'];
 
-        $data = $app->db->select('tbl_users', [
+        // $data = $app->db->select('tbl_users', [
+        //     "[><]tbl_finances" => "id_user"
+        // ], '*',[
+        //     "id_parent" => $id_parent,
+        //     'status' => "BELUM BAYAR"
+        // ]);
+
+        $totaldata = $app->db->sum('tbl_users', [
             "[><]tbl_finances" => "id_user"
-        ], '*',[
+        ], 'amount_payment',[
             "id_parent" => $id_parent,
             'status' => "BELUM BAYAR"
         ]);
-
-        $totaldata = count($data['amount_payment']);
-        // var_dump($data);
+        // var_dump($totaldata);
 
         $app->view->render($response, 'dashboard/parent.html', [
             'totaldata' => $totaldata,
-            'data' => $data,
             'user' => $user,
             'type' => $type,
             'user' => $user
