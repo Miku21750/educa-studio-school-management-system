6bc79a399a835170b2a82087e12fba3699648a3d
dev@localhost <dev@localhost>
2022-12-05 16:08:59 +0700

    savepoint

--- app/Controller/TeacherController.php
+++ app/Controller/TeacherController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 class TeacherController
 {
 
-    public static function dataTeacher($app, $request, $response, $args, $namaKolom)
+    public static function getTeacherData($app, $request, $response, $args, $namaKolom)
     {
 
         // id_user id_class id_hostel id_trans id_user_type id_parent first_name last_name
@@ -21,6 +21,17 @@ class TeacherController
         $response = $app->db->select("tbl_users", $namaKolom, [
             "id_user_type" => 2, 'id_user' => $_SESSION['id_user'],
         ]);
+
+        return $response;
+    }
+
+    public static function getTeacherSubject($app, $request, $response, $args)
+    {
+        $response = $app->db->query('select distinct tc.class as kelas, ts2.section, ts.subject_name as subject from tbl_users tu
+        left join tbl_classes tc on tu.id_class = tc.id_class
+        left join tbl_subjects ts on tc.id_section = ts.id_subject
+        left join tbl_sections ts2 on tc.id_section = ts2.id_section
+        where id_user_type = 2 and id_user = 23;')->fetchAll();
 
         return $response;
     }
@@ -44,16 +55,20 @@ class TeacherController
         // Alamat :    House #10, Road #6, Australia
         // No. Handpone :    + 88 98568888418
 
-        $namaDepanGuru = TeacherController::dataTeacher($app, $request, $response, $args, "first_name");
-        $namaBelakangGuru = TeacherController::dataTeacher($app, $request, $response, $args, "last_name");
-        $shortbioGuru = TeacherController::dataTeacher($app, $request, $response, $args, "gender");
-        $jenisKelamin = TeacherController::dataTeacher($app, $request, $response, $args, "religion");
-        $admissionDate = TeacherController::dataTeacher($app, $request, $response, $args, "admission_date");
-        $email = TeacherController::dataTeacher($app, $request, $response, $args, "email");
-        $NIP = TeacherController::dataTeacher($app, $request, $response, $args, "NISN");
-        $addressUser = TeacherController::dataTeacher($app, $request, $response, $args, "address_user");
-        $phoneUser = TeacherController::dataTeacher($app, $request, $response, $args, "phone_user");
-        $shortbioGuru = TeacherController::dataTeacher($app, $request, $response, $args, "short_bio");
+        $namaDepanGuru = TeacherController::getTeacherData($app, $request, $response, $args, "first_name");
+        $namaBelakangGuru = TeacherController::getTeacherData($app, $request, $response, $args, "last_name");
+        $shortbioGuru = TeacherController::getTeacherData($app, $request, $response, $args, "gender");
+        $jenisKelamin = TeacherController::getTeacherData($app, $request, $response, $args, "gender");
+        $agama = TeacherController::getTeacherData($app, $request, $response, $args, "religion");
+        $admissionDate = TeacherController::getTeacherData($app, $request, $response, $args, "admission_date");
+        $email = TeacherController::getTeacherData($app, $request, $response, $args, "email");
+        $NIP = TeacherController::getTeacherData($app, $request, $response, $args, "NISN");
+        $addressUser = TeacherController::getTeacherData($app, $request, $response, $args, "address_user");
+        $phoneUser = TeacherController::getTeacherData($app, $request, $response, $args, "phone_user");
+        $shortbioGuru = TeacherController::getTeacherData($app, $request, $response, $args, "short_bio");
+        $kelas = TeacherController::getTeacherSubject($app, $request, $response, $args);
+
+        // return var_dump($kelas);
 
         $app->view->render($response, 'teacher/teacher-details.html', [
             'namaDepanGuru' => $namaDepanGuru[0],
@@ -66,6 +81,8 @@ class TeacherController
             'addressUser' => $addressUser[0],
             'phoneUser' => $phoneUser[0],
             'shortbioGuru' => $shortbioGuru[0],
+            'agama' => $agama[0],
+            'kelas' => $kelas,
         ]);
 
         // return var_dump($namaDepanGuru[0]);
