bfa1c137bdfcaa227cfdb35a8c427e0d85552028
dev@localhost <dev@localhost>
2022-12-06 17:09:00 +0700

    savepoint

--- app/Controller/ClassController.php
+++ app/Controller/ClassController.php
@@ -5,23 +5,73 @@ namespace App\Controller;
 class ClassController
 {
 
-    function getTeacher($app, $request, $response, $args){
-        
+    public function getTeacherModel($app, $request, $response, $args)
+    {
+        $response = $app->db->select("tbl_users", [
+            "first_name",
+            "last_name",
+            "NISN",
+            "gender",
+            "phone_user",
+            "email",
+
+        ], [
+            "id_user_type" => 2,
+        ]);
+
+        return $response;
+
     }
 
-    public static function insertClass($app, $request, $response, $args){
+    public function getClassModel($app, $request, $response, $args)
+    {
+        $response = $app->db->select("tbl_classes", "*" );
+
+        return $response;
 
     }
 
+    public function getSectionModel($app, $request, $response, $args)
+    {
+        $response = $app->db->select("tbl_sections", "*" );
 
-    public static function viewAddClass($app, $request, $response, $args){
+        return $response;
+
+    }
+
+    public static function getClass($app, $request, $response, $args)
+    {
+        $response = $app->db->select("tbl_users", [
+            "first_name",
+            "last_name",
+            "NISN",
+            "gender",
+            "phone_user",
+            "email",
+
+        ], [
+            "id_user_type" => 2,
+        ]);
+
+        return $response;
+
+    }
+
+    public static function insertClass($app, $request, $response, $args)
+    {
+
+    }
+
+    public static function viewAddClass($app, $request, $response, $args)
+    {
+
+        return var_dump(ClassController::getSectionModel($app, $request, $response, $args));
+
         $app->view->render($response, 'class/add-class.html', [
             'test' => 'test',
 
-            
         ]);
 
     }
-    
 
 }
