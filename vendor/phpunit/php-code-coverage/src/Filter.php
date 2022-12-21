2a1fc90de78a17b55b1c37b350f54a98912ac060
CaturSkak <ahmadcaturyulianto77@gmail.com>
2022-12-21 15:54:42 +0700

    Revert "update vendor + add php .env"
    This reverts commit a65add4f23e6203e42604bc616aee6fda48b7f27.

--- vendor/phpunit/php-code-coverage/src/Filter.php
+++ vendor/phpunit/php-code-coverage/src/Filter.php
@@ -1,118 +1,173 @@
-<?php declare(strict_types=1);
+<?php
 /*
- * This file is part of phpunit/php-code-coverage.
+ * This file is part of the php-code-coverage package.
  *
  * (c) Sebastian Bergmann <sebastian@phpunit.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace SebastianBergmann\CodeCoverage;
 
-use function array_keys;
-use function is_file;
-use function realpath;
-use function strpos;
-use SebastianBergmann\FileIterator\Facade as FileIteratorFacade;
-
-final class Filter
+/**
+ * Filter for whitelisting of code coverage information.
+ */
+class Filter
 {
     /**
-     * @psalm-var array<string,true>
+     * Source files that are whitelisted.
+     *
+     * @var array
      */
-    private $files = [];
+    private $whitelistedFiles = [];
 
     /**
-     * @psalm-var array<string,bool>
+     * Adds a directory to the whitelist (recursively).
+     *
+     * @param string $directory
+     * @param string $suffix
+     * @param string $prefix
      */
-    private $isFileCache = [];
-
-    public function includeDirectory(string $directory, string $suffix = '.php', string $prefix = ''): void
+    public function addDirectoryToWhitelist($directory, $suffix = '.php', $prefix = '')
     {
-        foreach ((new FileIteratorFacade)->getFilesAsArray($directory, $suffix, $prefix) as $file) {
-            $this->includeFile($file);
+        $facade = new \File_Iterator_Facade;
+        $files  = $facade->getFilesAsArray($directory, $suffix, $prefix);
+
+        foreach ($files as $file) {
+            $this->addFileToWhitelist($file);
         }
-    }
-
-    /**
-     * @psalm-param list<string> $files
-     */
-    public function includeFiles(array $filenames): void
-    {
-        foreach ($filenames as $filename) {
-            $this->includeFile($filename);
-        }
-    }
-
-    public function includeFile(string $filename): void
-    {
-        $filename = realpath($filename);
-
-        if (!$filename) {
-            return;
-        }
-
-        $this->files[$filename] = true;
-    }
-
-    public function excludeDirectory(string $directory, string $suffix = '.php', string $prefix = ''): void
-    {
-        foreach ((new FileIteratorFacade)->getFilesAsArray($directory, $suffix, $prefix) as $file) {
-            $this->excludeFile($file);
-        }
-    }
-
-    public function excludeFile(string $filename): void
-    {
-        $filename = realpath($filename);
-
-        if (!$filename || !isset($this->files[$filename])) {
-            return;
-        }
-
-        unset($this->files[$filename]);
-    }
-
-    public function isFile(string $filename): bool
-    {
-        if (isset($this->isFileCache[$filename])) {
-            return $this->isFileCache[$filename];
-        }
-
-        if ($filename === '-' ||
-            strpos($filename, 'vfs://') === 0 ||
-            strpos($filename, 'xdebug://debug-eval') !== false ||
-            strpos($filename, 'eval()\'d code') !== false ||
-            strpos($filename, 'runtime-created function') !== false ||
-            strpos($filename, 'runkit created function') !== false ||
-            strpos($filename, 'assert code') !== false ||
-            strpos($filename, 'regexp code') !== false ||
-            strpos($filename, 'Standard input code') !== false) {
-            $isFile = false;
-        } else {
-            $isFile = is_file($filename);
-        }
-
-        $this->isFileCache[$filename] = $isFile;
-
-        return $isFile;
-    }
-
-    public function isExcluded(string $filename): bool
-    {
-        return !isset($this->files[$filename]) || !$this->isFile($filename);
     }
 
     /**
-     * @psalm-return list<string>
+     * Adds a file to the whitelist.
+     *
+     * @param string $filename
      */
-    public function files(): array
+    public function addFileToWhitelist($filename)
     {
-        return array_keys($this->files);
+        $this->whitelistedFiles[\realpath($filename)] = true;
     }
 
-    public function isEmpty(): bool
+    /**
+     * Adds files to the whitelist.
+     *
+     * @param array $files
+     */
+    public function addFilesToWhitelist(array $files)
     {
-        return empty($this->files);
+        foreach ($files as $file) {
+            $this->addFileToWhitelist($file);
+        }
+    }
+
+    /**
+     * Removes a directory from the whitelist (recursively).
+     *
+     * @param string $directory
+     * @param string $suffix
+     * @param string $prefix
+     */
+    public function removeDirectoryFromWhitelist($directory, $suffix = '.php', $prefix = '')
+    {
+        $facade = new \File_Iterator_Facade;
+        $files  = $facade->getFilesAsArray($directory, $suffix, $prefix);
+
+        foreach ($files as $file) {
+            $this->removeFileFromWhitelist($file);
+        }
+    }
+
+    /**
+     * Removes a file from the whitelist.
+     *
+     * @param string $filename
+     */
+    public function removeFileFromWhitelist($filename)
+    {
+        $filename = \realpath($filename);
+
+        unset($this->whitelistedFiles[$filename]);
+    }
+
+    /**
+     * Checks whether a filename is a real filename.
+     *
+     * @param string $filename
+     *
+     * @return bool
+     */
+    public function isFile($filename)
+    {
+        if ($filename == '-' ||
+            \strpos($filename, 'vfs://') === 0 ||
+            \strpos($filename, 'xdebug://debug-eval') !== false ||
+            \strpos($filename, 'eval()\'d code') !== false ||
+            \strpos($filename, 'runtime-created function') !== false ||
+            \strpos($filename, 'runkit created function') !== false ||
+            \strpos($filename, 'assert code') !== false ||
+            \strpos($filename, 'regexp code') !== false) {
+            return false;
+        }
+
+        return \file_exists($filename);
+    }
+
+    /**
+     * Checks whether or not a file is filtered.
+     *
+     * @param string $filename
+     *
+     * @return bool
+     */
+    public function isFiltered($filename)
+    {
+        if (!$this->isFile($filename)) {
+            return true;
+        }
+
+        $filename = \realpath($filename);
+
+        return !isset($this->whitelistedFiles[$filename]);
+    }
+
+    /**
+     * Returns the list of whitelisted files.
+     *
+     * @return array
+     */
+    public function getWhitelist()
+    {
+        return \array_keys($this->whitelistedFiles);
+    }
+
+    /**
+     * Returns whether this filter has a whitelist.
+     *
+     * @return bool
+     */
+    public function hasWhitelist()
+    {
+        return !empty($this->whitelistedFiles);
+    }
+
+    /**
+     * Returns the whitelisted files.
+     *
+     * @return array
+     */
+    public function getWhitelistedFiles()
+    {
+        return $this->whitelistedFiles;
+    }
+
+    /**
+     * Sets the whitelisted files.
+     *
+     * @param array $whitelistedFiles
+     */
+    public function setWhitelistedFiles($whitelistedFiles)
+    {
+        $this->whitelistedFiles = $whitelistedFiles;
     }
 }
--- vendor/phpunit/phpunit/src/Framework/ExceptionWrapper.php
+++ vendor/phpunit/phpunit/src/Framework/ExceptionWrapper.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 /*
  * This file is part of PHPUnit.
  *
@@ -9,13 +9,8 @@
  */
 namespace PHPUnit\Framework;
 
-use const PHP_VERSION_ID;
-use function array_keys;
-use function get_class;
-use function spl_object_hash;
 use PHPUnit\Util\Filter;
 use Throwable;
-use WeakReference;
 
 /**
  * Wraps Exceptions thrown by code under test.
@@ -23,12 +18,10 @@ use WeakReference;
  * Re-instantiates Exceptions thrown by user-space code to retain their original
  * class names, properties, and stack traces (but without arguments).
  *
- * Unlike PHPUnit\Framework\Exception, the complete stack of previous Exceptions
+ * Unlike PHPUnit\Framework_\Exception, the complete stack of previous Exceptions
  * is processed.
- *
- * @internal This class is not covered by the backward compatibility promise for PHPUnit
  */
-final class ExceptionWrapper extends Exception
+class ExceptionWrapper extends Exception
 {
     /**
      * @var string
@@ -36,25 +29,54 @@ final class ExceptionWrapper extends Exception
     protected $className;
 
     /**
-     * @var null|ExceptionWrapper
+     * @var ExceptionWrapper|null
      */
     protected $previous;
 
     /**
-     * @var null|WeakReference<Throwable>
+     * @param Throwable $t
      */
-    private $originalException;
-
     public function __construct(Throwable $t)
     {
         // PDOException::getCode() is a string.
-        // @see https://php.net/manual/en/class.pdoexception.php#95812
+        // @see http://php.net/manual/en/class.pdoexception.php#95812
         parent::__construct($t->getMessage(), (int) $t->getCode());
 
-        $this->setOriginalException($t);
+        $this->className = \get_class($t);
+        $this->file      = $t->getFile();
+        $this->line      = $t->getLine();
+
+        $this->serializableTrace = $t->getTrace();
+
+        foreach ($this->serializableTrace as $i => $call) {
+            unset($this->serializableTrace[$i]['args']);
+        }
+
+        if ($t->getPrevious()) {
+            $this->previous = new self($t->getPrevious());
+        }
     }
 
-    public function __toString(): string
+    /**
+     * @return string
+     */
+    public function getClassName()
+    {
+        return $this->className;
+    }
+
+    /**
+     * @return ExceptionWrapper
+     */
+    public function getPreviousWrapped()
+    {
+        return $this->previous;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
     {
         $string = TestFailure::exceptionToString($this);
 
@@ -67,72 +89,5 @@ final class ExceptionWrapper extends Exception
         }
 
         return $string;
-    }
-
-    public function getClassName(): string
-    {
-        return $this->className;
-    }
-
-    public function getPreviousWrapped(): ?self
-    {
-        return $this->previous;
-    }
-
-    public function setClassName(string $className): void
-    {
-        $this->className = $className;
-    }
-
-    public function setOriginalException(Throwable $t): void
-    {
-        $this->originalException($t);
-
-        $this->className = get_class($t);
-        $this->file      = $t->getFile();
-        $this->line      = $t->getLine();
-
-        $this->serializableTrace = $t->getTrace();
-
-        foreach (array_keys($this->serializableTrace) as $key) {
-            unset($this->serializableTrace[$key]['args']);
-        }
-
-        if ($t->getPrevious()) {
-            $this->previous = new self($t->getPrevious());
-        }
-    }
-
-    public function getOriginalException(): ?Throwable
-    {
-        return $this->originalException();
-    }
-
-    /**
-     * Method to contain static originalException to exclude it from stacktrace to prevent the stacktrace contents,
-     * which can be quite big, from being garbage-collected, thus blocking memory until shutdown.
-     *
-     * Approach works both for var_dump() and var_export() and print_r().
-     */
-    private function originalException(Throwable $exceptionToStore = null): ?Throwable
-    {
-        // drop once PHP 7.3 support is removed
-        if (PHP_VERSION_ID < 70400) {
-            static $originalExceptions;
-
-            $instanceId = spl_object_hash($this);
-
-            if ($exceptionToStore) {
-                $originalExceptions[$instanceId] = $exceptionToStore;
-            }
-
-            return $originalExceptions[$instanceId] ?? null;
-        }
-
-        if ($exceptionToStore) {
-            $this->originalException = WeakReference::create($exceptionToStore);
-        }
-
-        return $this->originalException !== null ? $this->originalException->get() : null;
     }
 }
